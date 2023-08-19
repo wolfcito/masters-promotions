b2965f76895fafbcd3851755b144749fe6a5a5b4
Luis Fernando <guffenix@gmail.com>
2023-08-18 23:49:47 -0500

    chore: improve global types

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,7 @@
-import {SVGProps} from "react";
+import { SVGProps } from 'react'
 
-export type IconSvgProps = SVGProps<SVGSVGElement> & {
-  size?: number;
-};
+export interface IconSvgProps
+  extends SVGProps<SVGSVGElement>,
+    JSX.IntrinsicAttributes {
+  size?: number
+}
