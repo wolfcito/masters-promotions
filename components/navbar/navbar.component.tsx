449e2046ae6537d91cfc7c16efd848a263ea98f3
Luis Fernando <guffenix@gmail.com>
2023-08-20 12:32:48 -0500

    feat: simple navbar

--- components/navbar/navbar.component.tsx
+++ components/navbar/navbar.component.tsx
@@ -1,54 +1,18 @@
 import {
   Navbar as NextUINavbar,
   NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
   NavbarBrand,
   NavbarItem,
-  NavbarMenuItem,
 } from '@nextui-org/navbar'
 import { Button } from '@nextui-org/button'
-import { Kbd } from '@nextui-org/kbd'
 import { Link } from '@nextui-org/link'
-import { Input } from '@nextui-org/input'
-
 import { link as linkStyles } from '@nextui-org/theme'
-
 import { siteConfig } from '~/config/site'
 import NextLink from 'next/link'
 import clsx from 'clsx'
 
 import { ThemeSwitch } from '~/components/theme-switch'
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-} from '~/components/icons'
-
-function SearchInput() {
-  return (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: 'bg-default-100',
-        input: 'text-sm',
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={['command']}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-default-400 pointer-events-none flex-shrink-0 text-base" />
-      }
-      type="search"
-    />
-  )
-}
+import { HeartFilledIcon } from '~/icons'
 
 export function Navbar() {
   return (
@@ -81,69 +45,18 @@ export function Navbar() {
         </ul>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden basis-1/5 sm:flex sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden gap-2 sm:flex">
-          <Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} aria-label="Github">
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">
-          <SearchInput />
-        </NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-default-600 bg-default-100 text-sm font-normal"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="basis-1 pl-4 sm:hidden" justify="end">
-        <Link isExternal href={siteConfig.links.github} aria-label="Github">
-          <GithubIcon className="text-default-500" />
-        </Link>
+      <NavbarContent className="basis-1 pl-4" justify="end">
         <ThemeSwitch />
-        <NavbarMenuToggle />
+        <Button
+          isIconOnly
+          isExternal
+          as={Link}
+          className="text-default-600 bg-default-100 text-sm font-normal"
+          href={siteConfig.links.sponsor}
+          startContent={<HeartFilledIcon className="text-danger" />}
+          variant="light"
+        ></Button>
       </NavbarContent>
-
-      <NavbarMenu>
-        <SearchInput />
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? 'primary'
-                    : index === siteConfig.navMenuItems.length - 1
-                    ? 'danger'
-                    : 'foreground'
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
     </NextUINavbar>
   )
 }
