bbf7e3efd0eff541815ec42c7e40a75902985d42
Luis Fernando <guffenix@gmail.com>
2023-08-20 23:46:47 -0500

    chore: about page updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,14 @@
-import { title } from "@/components/primitives";
+import { title } from '~/components/primitives'
 
 export default function AboutPage() {
-	return (
-		<div>
-			<h1 className={title()}>About</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className={title()}>Fūrinkazan</h1>
+
+      <p>
+        This platform is designed to honor and highlight the exceptional mastery
+        of martial arts instructors associated with the Fūrinkazan philosophy.
+      </p>
+    </div>
+  )
 }
