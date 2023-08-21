c3dbd8359867d50ed44b66dfb45194764f795a35
Luis Fernando <guffenix@gmail.com>
2023-08-21 00:28:05 -0500

    chore: update links for more projects

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,54 @@
 import { title } from '~/components/primitives'
+import NextLink from 'next/link'
+import { Link } from '@nextui-org/link'
+import { button as buttonStyles } from '@nextui-org/theme'
+import { siteConfig } from '~/config/site'
+import { GithubIcon } from '~/icons'
 
 export default function AboutPage() {
   return (
     <div>
-      <h1 className={title()}>Fūrinkazan</h1>
-      <br />
-      <br />
-      <span>
+      <h1 className={title({ className: 'mb-8 text-center' })}>Fūrinkazan</h1>
+
+      <span className="mb-3 flex">
         This platform is designed to honor and highlight the exceptional mastery
         of martial arts instructors associated with the Fūrinkazan philosophy.
       </span>
+      <div className="flex w-full flex-col gap-3">
+        <Link
+          isExternal
+          as={NextLink}
+          className={buttonStyles({ variant: 'bordered', radius: 'full' })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+        <Link
+          isExternal
+          as={NextLink}
+          href={siteConfig.links.docs}
+          className={buttonStyles({
+            color: 'primary',
+            radius: 'full',
+            variant: 'shadow',
+          })}
+        >
+          See more projects
+        </Link>
+      </div>
+
+      <div className="my-8 flex justify-center">
+        <Link
+          isExternal
+          className="flex items-center gap-1 text-current"
+          href="https://mundovirtual.solutions/"
+          title="Mundo Virtual Homepage"
+        >
+          <span className="text-default-600">Powered by</span>
+          <p className="text-primary">MundoVirtual</p>
+        </Link>
+      </div>
     </div>
   )
 }
