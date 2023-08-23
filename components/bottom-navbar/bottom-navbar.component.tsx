c765e87b8734144c14d63241b9e1e387312d750f
Luis Fernando <guffenix@gmail.com>
2023-08-23 10:43:51 -0500

    chore: representative kanjis of furinkazan enabled

--- components/bottom-navbar/bottom-navbar.component.tsx
+++ components/bottom-navbar/bottom-navbar.component.tsx
@@ -7,7 +7,10 @@ export function BottomNavbar({ categories }: BottomNavbarProps) {
     <section className="fixed bottom-0 z-10 flex w-full items-center justify-center bg-white/10 py-3 backdrop-blur-md">
       <div className="flex items-center gap-8">
         {categories.map((item) => (
-          <Avatar key={item.id} isBordered radius="md" src={item.image} />
+          <div key={item.id} className="flex flex-col">
+            <Avatar isBordered radius="md" src={item.image} />
+            <p className="text-center">{item.emoji}</p>
+          </div>
         ))}
       </div>
     </section>
