c6b027d73280f033e6f71bd7a6af9b6a55df9b21
Luis Fernando <guffenix@gmail.com>
2023-08-24 19:35:35 -0500

    chore: apply conventions about page

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,13 +1,11 @@
-export default function AboutLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				{children}
-			</div>
-		</section>
-	);
+import { AboutPageProps } from './about.type'
+
+export default function AboutLayout({ children }: AboutPageProps) {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg justify-center text-center">
+        {children}
+      </div>
+    </section>
+  )
 }
