ef632acf73e63429c128c6658fa41eff9740bc88
Luis Fernando <guffenix@gmail.com>
2023-08-24 19:38:58 -0500

    feat: filtering newsfeed for categories

--- services/photos.service.ts
+++ services/photos.service.ts
@@ -14,3 +14,19 @@ export const photos = async () => {
 
   return photos
 }
+
+export const photosByCategoryId = async ({ slug }: { slug: string }) => {
+  const { photos } = await clientGraphql.query({
+    photos: {
+      __args: { where: { category: { slug: { _eq: slug } } } },
+      id: true,
+      image: true,
+      liked: true,
+      likes: true,
+      user: { id: true, image: true, name: true, nickname: true },
+      category: { image: true, name: true },
+    },
+  })
+
+  return photos
+}
