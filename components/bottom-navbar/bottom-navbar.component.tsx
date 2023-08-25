958d3b59f9c6450239f78995434d69dd7e5cdf6e
Luis Fernando <guffenix@gmail.com>
2023-08-24 19:37:28 -0500

    feat: linking with caracteristic mastering

--- components/bottom-navbar/bottom-navbar.component.tsx
+++ components/bottom-navbar/bottom-navbar.component.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Avatar } from '@nextui-org/react'
+import { Avatar, Link } from '@nextui-org/react'
 import { BottomNavbarProps } from './bottom-navbar.type'
 
 export function BottomNavbar({ categories }: BottomNavbarProps) {
@@ -7,10 +7,14 @@ export function BottomNavbar({ categories }: BottomNavbarProps) {
     <section className="fixed bottom-0 z-10 flex w-full items-center justify-center bg-white/10 py-3 backdrop-blur-md">
       <div className="flex items-center gap-8">
         {categories.map((item) => (
-          <div key={item.id} className="flex flex-col">
-            <Avatar isBordered radius="md" src={item.image} />
-            <p className="text-center">{item.emoji}</p>
-          </div>
+          <Link key={item.id} href={item.slug}>
+            <div className="flex cursor-pointer flex-col">
+              <Avatar isBordered radius="md" src={item.image} />
+              <p className="text-center font-semibold text-stone-800 dark:text-slate-200">
+                {item.emoji}
+              </p>
+            </div>
+          </Link>
         ))}
       </div>
     </section>
